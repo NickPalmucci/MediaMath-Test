276401ae3f2c6412a20756a1448edfd4b3438cc6
Mucciano <nicolas.eli.palmucci@gmail.com>
2018-04-17 06:14:48 -0400

    compose url with base plus route string

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,14 +6,14 @@ import { Observable } from 'rxjs/Observable';
 
 export class DataService {
 
-  private dataUrl = 'http://localhost:5000/';
+  private baseUrl = 'http://localhost:5000';
 
   constructor(
     private http: HttpClient
   ) {}
 
-  getData(): Observable<object> {
-  return this.http.get(this.dataUrl)
+  getData(route: string): any {
+  return this.http.get(this.baseUrl + route);
   }
 
 }
