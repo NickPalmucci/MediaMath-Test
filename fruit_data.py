d4058cacd37cadf5ccb3d9b2579ec31d37867613
Mucciano <nicolas.eli.palmucci@gmail.com>
2018-04-17 12:40:45 -0400

    test data

---
+++ fruit_data.py
@@ -0,0 +1,123 @@
+
+f_data = [
+    {
+        "Food": "Apples",
+        "portionSize": 5.2,
+        "pricePortionRatio": 0.36,
+        "caloriesPortionRatio": 77
+    },
+    {
+        "Food": "Applesauce, jarred",
+        "portionSize": 4.6,
+        "pricePortionRatio": 0.22,
+        "caloriesPortionRatio": 100
+    },
+    {
+        "Food": "Bananas",
+        "portionSize": 4.1,
+        "pricePortionRatio": 0.18,
+        "caloriesPortionRatio": 102
+    },
+    {
+        "Food": "Cantaloupe",
+        "portionSize": 3.5,
+        "pricePortionRatio": 0.38,
+        "caloriesPortionRatio": 33
+    },
+    {
+        "Food": "Fruit cocktail, canned*",
+        "portionSize": 4.4,
+        "pricePortionRatio": 0.31,
+        "caloriesPortionRatio": 71
+    },
+    {
+        "Food": "Grapes",
+        "portionSize": 3,
+        "pricePortionRatio": 0.32,
+        "caloriesPortionRatio": 59
+    },
+    {
+        "Food": "Oranges, navel",
+        "portionSize": 3.8,
+        "pricePortionRatio": 0.2,
+        "caloriesPortionRatio": 53
+    },
+    {
+        "Food": "Peaches, canned*",
+        "portionSize": 4.4,
+        "pricePortionRatio": 0.31,
+        "caloriesPortionRatio": 68
+    },
+    {
+        "Food": "Pineapple, canned*",
+        "portionSize": 4.4,
+        "pricePortionRatio": 0.26,
+        "caloriesPortionRatio": 75
+    },
+    {
+        "Food": "Plum*",
+        "portionSize": 2.9,
+        "pricePortionRatio": 0.25,
+        "caloriesPortionRatio": 38
+    },
+    {
+        "Food": "Raisins*",
+        "portionSize": 1.3,
+        "pricePortionRatio": 0.19,
+        "caloriesPortionRatio": 109
+    },
+    {
+        "Food": "Strawberries*",
+        "portionSize": 2.9,
+        "pricePortionRatio": 0.41,
+        "caloriesPortionRatio": 27
+    },
+    {
+        "Food": "Tangerines",
+        "portionSize": 4.8,
+        "pricePortionRatio": 0.51,
+        "caloriesPortionRatio": 72
+    },
+    {
+        "Food": "Watermelon",
+        "portionSize": 8.7,
+        "pricePortionRatio": 0.25,
+        "caloriesPortionRatio": 74
+    },
+    {
+        "Food": "Broccoli florets*",
+        "portionSize": 1.6,
+        "pricePortionRatio": 0.18,
+        "caloriesPortionRatio": 12
+    },
+    {
+        "Food": "Carrots, baby*",
+        "portionSize": 2.3,
+        "pricePortionRatio": 0.19,
+        "caloriesPortionRatio": 22
+    },
+    {
+        "Food": "Celery*",
+        "portionSize": 2.1,
+        "pricePortionRatio": 0.16,
+        "caloriesPortionRatio": 10
+    },
+    {
+        "Food": "Red peppers*",
+        "portionSize": 2.6,
+        "pricePortionRatio": 0.6,
+        "caloriesPortionRatio": 23
+    },
+    {
+        "Food": "Sweet potatoes, cooked*",
+        "portionSize": 3.5,
+        "pricePortionRatio": 0.33,
+        "caloriesPortionRatio": 90
+    },
+    {
+        "Food": "Tomatoes, grape/cherry*",
+        "portionSize": 3.2,
+        "pricePortionRatio": 0.55,
+        "caloriesPortionRatio": 16
+    }
+]
---
+++ snack_data.py
@@ -0,0 +1,123 @@
+
+s_data = [
+    {
+        "Food": "Chocolate  candy",
+        "portionSize": 0.9,
+        "pricePortionRatio": 0.24,
+        "caloriesPortionRatio": 262
+    },
+    {
+        "Food": "Cookies",
+        "portionSize": 1,
+        "pricePortionRatio": 0.16,
+        "caloriesPortionRatio": 123
+    },
+    {
+        "Food": "Corn chips",
+        "portionSize": 1,
+        "pricePortionRatio": 0.21,
+        "caloriesPortionRatio": 140
+    },
+    {
+        "Food": "Crackers",
+        "portionSize": 0.9,
+        "pricePortionRatio": 0.16,
+        "caloriesPortionRatio": 114
+    },
+    {
+        "Food": "Cupcakes",
+        "portionSize": 2,
+        "pricePortionRatio": 0.34,
+        "caloriesPortionRatio": 174
+    },
+    {
+        "Food": "Danish",
+        "portionSize": 2.6,
+        "pricePortionRatio": 0.47,
+        "caloriesPortionRatio": 271
+    },
+    {
+        "Food": "Donuts",
+        "portionSize": 2.1,
+        "pricePortionRatio": 0.36,
+        "caloriesPortionRatio": 235
+    },
+    {
+        "Food": "Fruit rolls",
+        "portionSize": 0.8,
+        "pricePortionRatio": 0.28,
+        "caloriesPortionRatio": 82
+    },
+    {
+        "Food": "Graham crackers",
+        "portionSize": 0.9,
+        "pricePortionRatio": 0.14,
+        "caloriesPortionRatio": 102
+    },
+    {
+        "Food": "Granola  bars",
+        "portionSize": 1.1,
+        "pricePortionRatio": 0.3,
+        "caloriesPortionRatio": 119
+    },
+    {
+        "Food": "Ice cream",
+        "portionSize": 3.8,
+        "pricePortionRatio": 0.39,
+        "caloriesPortionRatio": 196
+    },
+    {
+        "Food": "Muffins",
+        "portionSize": 3.3,
+        "pricePortionRatio": 0.83,
+        "caloriesPortionRatio": 369
+    },
+    {
+        "Food": "Pizza, from frozen",
+        "portionSize": 3.3,
+        "pricePortionRatio": 0.63,
+        "caloriesPortionRatio": 252
+    },
+    {
+        "Food": "Popsicles and bars",
+        "portionSize": "1 count",
+        "pricePortionRatio": 0.34,
+        "caloriesPortionRatio": 80
+    },
+    {
+        "Food": "Potato chips",
+        "portionSize": 1.1,
+        "pricePortionRatio": 0.27,
+        "caloriesPortionRatio": 169
+    },
+    {
+        "Food": "Pretzels",
+        "portionSize": 1.5,
+        "pricePortionRatio": 0.25,
+        "caloriesPortionRatio": 168
+    },
+    {
+        "Food": "Pudding, ready-to-eat",
+        "portionSize": 4.1,
+        "pricePortionRatio": 0.38,
+        "caloriesPortionRatio": 152
+    },
+    {
+        "Food": "Sandwich crackers",
+        "portionSize": 1.3,
+        "pricePortionRatio": 0.2,
+        "caloriesPortionRatio": 183
+    },
+    {
+        "Food": "Toaster pastries",
+        "portionSize": 2.7,
+        "pricePortionRatio": 0.35,
+        "caloriesPortionRatio": 299
+    },
+    {
+        "Food": "Tortilla chips",
+        "portionSize": 1.2,
+        "pricePortionRatio": 0.21,
+        "caloriesPortionRatio": 161
+    }
+]
