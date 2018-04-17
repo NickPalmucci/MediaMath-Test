d03928314fd6f3d14698d7e0e5ac3225edf3f91e
Mucciano <nicolas.eli.palmucci@gmail.com>
2018-04-17 06:06:40 -0400

    remove unsued modules and refactor to reflect renaming of modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ApplicationRef } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
-import { Histogram } from './histogram/histo.component';
+import { FruitsComponent } from './fruits/fruits.component';
+import { HeaderComponent } from './header/header.component';
+import { CardComponent } from './card/card.component';
+import { SnacksComponent } from './snacks/snacks.component';
 
 import { D3Service } from 'd3-ng2-service';
 import { DataService } from './data.service';
-
 import { ChartsService } from './charts.service';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    Histogram
+    FruitsComponent,
+    HeaderComponent,
+    CardComponent,
+    SnacksComponent
   ],
   imports: [
     BrowserModule,
